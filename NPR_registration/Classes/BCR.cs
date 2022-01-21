ca3e7724e43845227fa6726e7ffe460dea5c9668
Netanel Mülbert - DSKTP <netanelmul@gmail.com>
2022-01-21 15:49:10 +0200

    BCR registration + same compName handling

---
+++ NPR_registration/Classes/BCR.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPR_registration.Classes
+{
+    public class BCR
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string compName { get; set; }
+        public string email { get; set; }
+        public string website { get; set; }
+        public string password { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ NPR_registration/Classes/BCRcreator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPR_registration.Classes
+{
+    public class BCRcreator
+    {
+        public static BCR CreateBCR(dynamic data)
+        {
+
+            dynamic BCRdata = data;
+
+            BCR newBCR = new BCR();
+            newBCR.compName = BCRdata.compName;
+            newBCR.email = BCRdata.email;
+            newBCR.website = BCRdata.website;
+            newBCR.password = BCRdata.password;
+
+            return newBCR;
+        }
+    }
+}
