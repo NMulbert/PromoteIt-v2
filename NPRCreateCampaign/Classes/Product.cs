3e712dda94d0173ec9a097dea01b3516039f6345
Netanel Mulbert <netanelmul@gmail.com>
2022-01-24 10:46:50 +0200

    Add product function

---
+++ NPRCreateCampaign/Classes/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPRCreateCampaign.Classes
+{
+    public class Product
+    {
+        public string product { get; set; }
+        public int productCount { get; set; }
+        public int price { get; set; }
+        public string compName { get; set; }
+    }
+}
