e70188beb76d143fea48884e4179150fe22e5230
Vlad Probst <vladlen.probst@gmail.com>
2022-01-26 20:34:52 +0200

    Time trigger version 3

---
+++ TimeTrigerUpadteTweetsAndBalance/UpdateTweetsAndBalance.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace TimeTrigerUpadteTweetsAndBalance
+{
+    public class UpdateTweetsAndBalance
+    {
+        [FunctionName("UpdateTweetsAndBalance")]
+        public async Task RunAsync([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            HttpClient updateBalanceOnTweet = new HttpClient();
+            string updateBalanceOnTweetUrl = "http://localhost:7731/api/UpdateBalanceOnTweet";
+            HttpResponseMessage response = await updateBalanceOnTweet.GetAsync(updateBalanceOnTweetUrl);
+            log.LogInformation(response.StatusCode.ToString());
+
+        }
+    }
+}
