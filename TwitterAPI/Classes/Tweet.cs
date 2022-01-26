f27c64400a20e308151b814409d560a7f7891e41
Netanel Mülbert <93646852+NMulbert@users.noreply.github.com>
2022-01-26 12:45:59 +0200

    Update Balance on tweet - stage 1

---
+++ TwitterAPI/Classes/Tweet.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterAPI.Classes
+{
+    public class Tweet
+    {
+        [JsonProperty(PropertyName = "id")]
+        public long tweetId { get; set; }
+        public string userName { get; set; }
+        public string hashTag { get; set; }
+        public string campaignLink { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
---
+++ TwitterAPI/Classes/TweetCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi.Models;
+
+namespace TwitterAPI.Classes
+{
+    public class TweetCreator
+    {
+        public static Tweet CreateTweet(ITweet tweet, string name, string hashtag,string link)
+        {
+            Tweet newTweet = new Tweet();
+            newTweet.tweetId = tweet.Id;
+            newTweet.userName = name;
+            newTweet.hashTag = hashtag;
+            newTweet.campaignLink = link;
+            
+            return newTweet;
+        }
+    }
+}
