7cb1d7792e051b4a5a4c58e407ac25edfc6e2cbe
Vlad Probst <vladlen.probst@gmail.com>
2022-01-27 19:11:46 +0200

    BCR Update and get order woks missing checks for update and get order doent triggerd after update

--- Login_SignIn/BCR-MainUI.Designer.cs
+++ Login_SignIn/BCR-MainUI.Designer.cs
@@ -43,7 +43,14 @@
             this.lblProductError = new System.Windows.Forms.Label();
             this.lblCountError = new System.Windows.Forms.Label();
             this.lblPriceError = new System.Windows.Forms.Label();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.lblOrders = new System.Windows.Forms.Label();
+            this.btnGetOrders = new System.Windows.Forms.Button();
+            this.lblOrderToUpdate = new System.Windows.Forms.Label();
+            this.btnUpdateStatus = new System.Windows.Forms.Button();
+            this.lblItemSelected = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailableCampaigns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvAvailableCampaigns
@@ -54,95 +61,101 @@
             this.dgvAvailableCampaigns.AllowUserToResizeRows = false;
             this.dgvAvailableCampaigns.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
             this.dgvAvailableCampaigns.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAvailableCampaigns.Location = new System.Drawing.Point(12, 59);
+            this.dgvAvailableCampaigns.Location = new System.Drawing.Point(10, 44);
+            this.dgvAvailableCampaigns.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvAvailableCampaigns.MultiSelect = false;
             this.dgvAvailableCampaigns.Name = "dgvAvailableCampaigns";
             this.dgvAvailableCampaigns.ReadOnly = true;
             this.dgvAvailableCampaigns.RowHeadersWidth = 51;
             this.dgvAvailableCampaigns.RowTemplate.Height = 29;
             this.dgvAvailableCampaigns.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dgvAvailableCampaigns.Size = new System.Drawing.Size(634, 229);
+            this.dgvAvailableCampaigns.Size = new System.Drawing.Size(555, 172);
             this.dgvAvailableCampaigns.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Location = new System.Drawing.Point(10, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.Size = new System.Drawing.Size(188, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Available Campaigns";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 319);
+            this.label2.Location = new System.Drawing.Point(10, 239);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.Size = new System.Drawing.Size(97, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Campaign Name";
             // 
             // txtCampaignName
             // 
-            this.txtCampaignName.Location = new System.Drawing.Point(12, 343);
+            this.txtCampaignName.Location = new System.Drawing.Point(10, 257);
+            this.txtCampaignName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCampaignName.Name = "txtCampaignName";
-            this.txtCampaignName.Size = new System.Drawing.Size(177, 27);
+            this.txtCampaignName.Size = new System.Drawing.Size(155, 23);
             this.txtCampaignName.TabIndex = 3;
             // 
             // txtProduct
             // 
-            this.txtProduct.Location = new System.Drawing.Point(195, 343);
+            this.txtProduct.Location = new System.Drawing.Point(171, 257);
+            this.txtProduct.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtProduct.Name = "txtProduct";
-            this.txtProduct.Size = new System.Drawing.Size(177, 27);
+            this.txtProduct.Size = new System.Drawing.Size(155, 23);
             this.txtProduct.TabIndex = 4;
             // 
             // txtCount
             // 
-            this.txtCount.Location = new System.Drawing.Point(378, 342);
+            this.txtCount.Location = new System.Drawing.Point(331, 256);
+            this.txtCount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCount.Name = "txtCount";
-            this.txtCount.Size = new System.Drawing.Size(177, 27);
+            this.txtCount.Size = new System.Drawing.Size(155, 23);
             this.txtCount.TabIndex = 5;
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(561, 342);
+            this.txtPrice.Location = new System.Drawing.Point(491, 256);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPrice.Name = "txtPrice";
-            this.txtPrice.Size = new System.Drawing.Size(177, 27);
+            this.txtPrice.Size = new System.Drawing.Size(155, 23);
             this.txtPrice.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(195, 319);
+            this.label3.Location = new System.Drawing.Point(171, 239);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.Size = new System.Drawing.Size(49, 15);
             this.label3.TabIndex = 7;
             this.label3.Text = "Product";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(378, 319);
+            this.label4.Location = new System.Drawing.Point(331, 239);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.Size = new System.Drawing.Size(40, 15);
             this.label4.TabIndex = 8;
             this.label4.Text = "Count";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(561, 319);
+            this.label5.Location = new System.Drawing.Point(491, 239);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 20);
+            this.label5.Size = new System.Drawing.Size(33, 15);
             this.label5.TabIndex = 9;
             this.label5.Text = "Price";
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(744, 341);
+            this.btnAdd.Location = new System.Drawing.Point(651, 256);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.Size = new System.Drawing.Size(82, 22);
             this.btnAdd.TabIndex = 10;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -151,40 +164,108 @@
             // lblCampaignNameError
             // 
             this.lblCampaignNameError.AutoSize = true;
-            this.lblCampaignNameError.Location = new System.Drawing.Point(12, 373);
+            this.lblCampaignNameError.Location = new System.Drawing.Point(10, 280);
             this.lblCampaignNameError.Name = "lblCampaignNameError";
-            this.lblCampaignNameError.Size = new System.Drawing.Size(0, 20);
+            this.lblCampaignNameError.Size = new System.Drawing.Size(0, 15);
             this.lblCampaignNameError.TabIndex = 11;
             // 
             // lblProductError
             // 
             this.lblProductError.AutoSize = true;
-            this.lblProductError.Location = new System.Drawing.Point(195, 372);
+            this.lblProductError.Location = new System.Drawing.Point(171, 279);
             this.lblProductError.Name = "lblProductError";
-            this.lblProductError.Size = new System.Drawing.Size(0, 20);
+            this.lblProductError.Size = new System.Drawing.Size(0, 15);
             this.lblProductError.TabIndex = 12;
             // 
             // lblCountError
             // 
             this.lblCountError.AutoSize = true;
-            this.lblCountError.Location = new System.Drawing.Point(378, 372);
+            this.lblCountError.Location = new System.Drawing.Point(331, 279);
             this.lblCountError.Name = "lblCountError";
-            this.lblCountError.Size = new System.Drawing.Size(0, 20);
+            this.lblCountError.Size = new System.Drawing.Size(0, 15);
             this.lblCountError.TabIndex = 13;
             // 
             // lblPriceError
             // 
             this.lblPriceError.AutoSize = true;
-            this.lblPriceError.Location = new System.Drawing.Point(561, 372);
+            this.lblPriceError.Location = new System.Drawing.Point(491, 279);
             this.lblPriceError.Name = "lblPriceError";
-            this.lblPriceError.Size = new System.Drawing.Size(0, 20);
+            this.lblPriceError.Size = new System.Drawing.Size(0, 15);
             this.lblPriceError.TabIndex = 14;
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.AllowUserToAddRows = false;
+            this.dgvOrders.AllowUserToDeleteRows = false;
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Location = new System.Drawing.Point(760, 44);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.ReadOnly = true;
+            this.dgvOrders.RowTemplate.Height = 25;
+            this.dgvOrders.Size = new System.Drawing.Size(413, 172);
+            this.dgvOrders.TabIndex = 15;
+            this.dgvOrders.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvOrders_CellClick);
+            // 
+            // lblOrders
+            // 
+            this.lblOrders.AutoSize = true;
+            this.lblOrders.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.lblOrders.Location = new System.Drawing.Point(760, 18);
+            this.lblOrders.Name = "lblOrders";
+            this.lblOrders.Size = new System.Drawing.Size(100, 25);
+            this.lblOrders.TabIndex = 16;
+            this.lblOrders.Text = "My Orders";
+            // 
+            // btnGetOrders
+            // 
+            this.btnGetOrders.Location = new System.Drawing.Point(760, 257);
+            this.btnGetOrders.Name = "btnGetOrders";
+            this.btnGetOrders.Size = new System.Drawing.Size(124, 23);
+            this.btnGetOrders.TabIndex = 17;
+            this.btnGetOrders.Text = "Get My Orders";
+            this.btnGetOrders.UseVisualStyleBackColor = true;
+            this.btnGetOrders.Click += new System.EventHandler(this.btnGetOrders_Click);
+            // 
+            // lblOrderToUpdate
+            // 
+            this.lblOrderToUpdate.AutoSize = true;
+            this.lblOrderToUpdate.Location = new System.Drawing.Point(954, 229);
+            this.lblOrderToUpdate.Name = "lblOrderToUpdate";
+            this.lblOrderToUpdate.Size = new System.Drawing.Size(93, 15);
+            this.lblOrderToUpdate.TabIndex = 18;
+            this.lblOrderToUpdate.Text = "Order To Update";
+            // 
+            // btnUpdateStatus
+            // 
+            this.btnUpdateStatus.Location = new System.Drawing.Point(1044, 257);
+            this.btnUpdateStatus.Name = "btnUpdateStatus";
+            this.btnUpdateStatus.Size = new System.Drawing.Size(129, 23);
+            this.btnUpdateStatus.TabIndex = 19;
+            this.btnUpdateStatus.Text = "Update Selected Order Status";
+            this.btnUpdateStatus.UseVisualStyleBackColor = true;
+            this.btnUpdateStatus.Click += new System.EventHandler(this.btnUpdateStatus_Click);
+            // 
+            // lblItemSelected
+            // 
+            this.lblItemSelected.AutoSize = true;
+            this.lblItemSelected.Location = new System.Drawing.Point(760, 229);
+            this.lblItemSelected.Name = "lblItemSelected";
+            this.lblItemSelected.Size = new System.Drawing.Size(173, 15);
+            this.lblItemSelected.TabIndex = 20;
+            this.lblItemSelected.Text = "Select id of the order to update:";
+           
             // 
             // BCR_MainUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1354, 518);
+            this.ClientSize = new System.Drawing.Size(1185, 388);
+            this.Controls.Add(this.lblItemSelected);
+            this.Controls.Add(this.btnUpdateStatus);
+            this.Controls.Add(this.lblOrderToUpdate);
+            this.Controls.Add(this.btnGetOrders);
+            this.Controls.Add(this.lblOrders);
+            this.Controls.Add(this.dgvOrders);
             this.Controls.Add(this.lblPriceError);
             this.Controls.Add(this.lblCountError);
             this.Controls.Add(this.lblProductError);
@@ -200,9 +281,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvAvailableCampaigns);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "BCR_MainUI";
             this.Text = "BCR_MainUI";
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailableCampaigns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,5 +308,11 @@
         private Label lblProductError;
         private Label lblCountError;
         private Label lblPriceError;
+        private DataGridView dgvOrders;
+        private Label lblOrders;
+        private Button btnGetOrders;
+        private Label lblOrderToUpdate;
+        private Button btnUpdateStatus;
+        private Label lblItemSelected;
     }
 }
