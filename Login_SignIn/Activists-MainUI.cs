4b691cfbc4e05690fed45d754d93c4bbac41ac1c
Netanel Mülbert <93646852+NMulbert@users.noreply.github.com>
2022-01-27 21:50:03 +0200

    Activist Main UI - get active campaigns + balance

---
+++ Login_SignIn/Activists-MainUI.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using NPRCreateCampaign.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_SignIn
+{
+    public partial class Activists_MainUI : Form
+    {
+        public Activists_MainUI()
+        {
+            InitializeComponent();
+
+            GetAllActiveCampaignsAsync();
+
+            GetUserBalanceAsync(frmLogin.name);
+        }
+
+        private async Task GetUserBalanceAsync(string activistName)
+        {
+            HttpClient client = new HttpClient();
+            var url = $"http://localhost:7734/api/GetBalance?name={activistName}";
+            string response = await client.GetStringAsync(url);
+            lblBalance.Text = response;
+        }
+
+        private async Task GetAllActiveCampaignsAsync()
+        {
+            List<Campaign>allActiveCampaigns = new List<Campaign>();    
+
+            HttpClient client = new HttpClient();
+            var url = "http://localhost:7730/api/GetProducts";
+
+            string response = await client.GetStringAsync(url);
+            allActiveCampaigns = JsonConvert.DeserializeObject<List<Campaign>>(response);
+
+            dgvAllActiveCampaigns.DataSource = allActiveCampaigns;
+        }
+    }
+}
