94849742e84ab339ca8929dc645dd05c4458ed7e
Netanel Mülbert <93646852+NMulbert@users.noreply.github.com>
2022-01-27 22:46:16 +0200

    Activist Main UIv2 - Work in progress

--- Login_SignIn/Activists-MainUI.cs
+++ Login_SignIn/Activists-MainUI.cs
@@ -14,6 +14,7 @@ namespace Login_SignIn
 {
     public partial class Activists_MainUI : Form
     {
+        
         public Activists_MainUI()
         {
             InitializeComponent();
@@ -21,6 +22,29 @@ namespace Login_SignIn
             GetAllActiveCampaignsAsync();
 
             GetUserBalanceAsync(frmLogin.name);
+
+        }
+
+        private async Task GetAllActiveCampaignsAsync()
+        {
+            List<Campaign> allActiveCampaigns = new List<Campaign>();
+
+            HttpClient client = new HttpClient();
+            var url = "http://localhost:7730/api/GetProducts";
+
+            string response = await client.GetStringAsync(url);
+            allActiveCampaigns = JsonConvert.DeserializeObject<List<Campaign>>(response);
+
+            dgvAllActiveCampaigns.DataSource = allActiveCampaigns;
+        }
+        private async Task UpdateBalanceOnLoginAsync()
+        {
+            HttpClient client = new HttpClient();
+            var url = "http://localhost:7731/api/UpdateBalanceOnTweet";
+            HttpResponseMessage respons = await client.GetAsync(url);
+            MessageBox.Show(respons.StatusCode.ToString());
+
+
         }
 
         private async Task GetUserBalanceAsync(string activistName)
@@ -31,17 +55,6 @@ namespace Login_SignIn
             lblBalance.Text = response;
         }
 
-        private async Task GetAllActiveCampaignsAsync()
-        {
-            List<Campaign>allActiveCampaigns = new List<Campaign>();    
 
-            HttpClient client = new HttpClient();
-            var url = "http://localhost:7730/api/GetProducts";
-
-            string response = await client.GetStringAsync(url);
-            allActiveCampaigns = JsonConvert.DeserializeObject<List<Campaign>>(response);
-
-            dgvAllActiveCampaigns.DataSource = allActiveCampaigns;
-        }
     }
 }
